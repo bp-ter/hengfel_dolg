36ad4266e3808c1739f38dba5206ccaf83a97a4c
Berta Péter <bertapeter@ktch.hu>
2025-04-09 14:54:51 +0200

    Teljesen kész

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,3 +1,13 @@
+/*
+* File: MainController.java
+* Author: Berta Péter
+* Copyright: 2025, Berta Péter
+* Group: Szoft II-2-N
+* Date: 2025-04-09
+* Github: https://github.com/bp-ter/
+* Licenc: MIT
+*/
+
 package com.example;
 
 import javafx.event.ActionEvent;
